cdb4a57d8d660db4ad7bca593e285694bdb462e8
Ibrahim Ethem Gursoy <iegursoy28@gmail.com>
2018-11-03 19:45:51 +0300

    initial commit

---
+++ FlightSimulatorServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlightSimulatorServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
